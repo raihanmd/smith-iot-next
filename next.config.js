b27445dfcd29d3feec4bc50da9211f4fad768b6d
raihanmd <mraihanf471@gmail.com>
2024-02-25 11:54:51 +0700

    bug: pwa icon bug cant download, popup didnt show up

--- next.config.js
+++ next.config.js
@@ -4,22 +4,20 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.output.hotUpdateMainFilename = 'static/webpack/[fullhash].[runtime].hot-update.json';
+      config.output.hotUpdateMainFilename =
+        "static/webpack/[fullhash].[runtime].hot-update.json";
     }
 
     return config;
   },
-  reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true, // Enable SWC minification for improved performance
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
-// Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 });
 
-// Export the combined configuration for Next.js with PWA support
 module.exports = withPWA(nextConfig);
